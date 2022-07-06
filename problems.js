8096123ffd4e018d3b578cff72672cdb3fa3dd2a
Austin Chao <austinschao@gmail.com>
2022-07-06 09:06:59 -0700

    1672. Richest Customer Wealth

--- problems.js
+++ problems.js
@@ -128,3 +128,25 @@ function decodeMessage(key, message) {
 
   return decodedMessage;
 }
+
+/*
+1672. Richest Customer Wealth
+
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+*/
+
+function maxiumumWealth(accounts) {
+  let maxWealth = 0;
+
+  for (let i = 0; i < accounts.length; i++) {
+    let tempMaxWealth = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      tempMaxWealth += accounts[i][j];
+    }
+    maxWealth = Math.max(tempMaxWealth, maxWealth);
+  }
+  return maxWealth;
+}
