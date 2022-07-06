19d70a4063289a2215561224464c34a59bf9fc09
Austin Chao <austinschao@gmail.com>
2022-07-06 15:57:33 -0700

    121. Best Time to Buy and Sell Stock

--- problems.js
+++ problems.js
@@ -179,3 +179,28 @@ function nonOverlappingIntervals(intervals) {
   }
   return minIntervals;
 }
+
+/*
+121. Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+*/
+function maxProfit(prices) {
+  let maxProfit = 0;
+  let left = 0;
+  let right = 1;
+
+  while (left < prices.length - 1) {
+    if (prices[left] < prices[right]) {
+      maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return maxProfit;
+}
