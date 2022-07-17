2617dbbd3279c66a97713f9b9749230c4e94525d
Austin Chao <austinschao@gmail.com>
2022-07-17 10:25:18 -0700

    268. Missing Number

--- problems.js
+++ problems.js
@@ -708,3 +708,18 @@ function inorderTraversal(root) {
 
   return result;
 }
+
+/*
+268. Missing Number
+Given an array of nums containing n distinct numbers in the range [0,n], return the only number in the range that is missing from the array.
+*/
+
+function missingNumber(nums) {
+  let sum = 0, total = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    total += i + 1;
+  }
+  return total - sum;
+};
