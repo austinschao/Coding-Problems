8ea105619b644f8f9343c9ce387a0bf00fb37fd6
Austin Chao <austinschao@gmail.com>
2022-07-17 11:47:20 -0700

    104. Maximum Depth of a Binary Tree

--- problems.js
+++ problems.js
@@ -770,3 +770,22 @@ function plusOne(digits) {
   }
   return digits;
 };
+
+/*
+104. Maximum Depth of a Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
+
+function maxDepth(root) {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  //     Helper function
+  //     function traverse(root) {
+  //     if (!root) return 0;
+  //     return 1 + Math.max(traverse(root.left), traverse(root.right));
+  // }
+  // return traverse(root);
+};
