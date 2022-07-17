709fd33dd9cc334e7e582ca132ab407756ecb064
Austin Chao <austinschao@gmail.com>
2022-07-17 12:10:13 -0700

    108. Convert Sorted Array to BST

--- problems.js
+++ problems.js
@@ -789,3 +789,22 @@ function maxDepth(root) {
   // }
   // return traverse(root);
 };
+
+/*
+108. Convert Sorted Array to Binary Search Tree
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+*/
+
+function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
+  let mid = Math.floor(nums.length / 2);
+  let node = new TreeNode(nums[mid]);
+
+  node.left = sortedArrayToBST(nums.slice(0, mid));
+  node.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
+
+  return node;
+};
