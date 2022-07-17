50dc2595a02d94a5cf4d482d28d336c5a4c9b4e8
Austin Chao <austinschao@gmail.com>
2022-07-17 11:11:10 -0700

    66. Plus One

--- problems.js
+++ problems.js
@@ -745,3 +745,28 @@ function removeDuplicates(nums) {
   }
   return nums.length;
 };
+
+/*
+66. Plus One
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+*/
+
+function plusOne(digits) {
+  let carryOver = true;
+
+  for (let i = digits.length - 1; i >= 0 && carryOver; i--) {
+    digits[i]++;
+    if (digits[i] >= 10) {
+      digits[i] = 0;
+    } else {
+      carryOver = false;
+    }
+  }
+  if (carryOver) {
+    digits.unshift(1);
+  }
+  return digits;
+};
