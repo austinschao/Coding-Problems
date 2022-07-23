70ea0e09b6cd4e92690cf1c4bc93621299706103
Austin Chao <austinschao@gmail.com>
2022-07-23 10:46:23 -0700

    463. Island Perimter

--- problems.js
+++ problems.js
@@ -833,3 +833,41 @@ function binaryTreePaths(root) {
   }
   return paths;
 };
+
+/*
+463. Island Perimeter
+
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+*/
+
+function islandPerimeter(grid) {
+  let perimeter = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        // check left
+        if (grid[i][j - 1] !== 1) {
+          perimeter += 1;
+        }
+        // check top
+        if (i - 1 < 0 || grid[i - 1][j] !== 1) {
+          perimeter += 1;
+        }
+        // check right
+        if (grid[i][j + 1] !== 1) {
+          perimeter += 1;
+        }
+        //check bottom
+        if (i + 1 === grid.length || grid[i + 1][j] !== 1) {
+          perimeter += 1;
+        }
+      }
+    }
+  }
+  return perimeter;
+};
