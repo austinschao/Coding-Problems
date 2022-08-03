de5e0cc10bf191a9fe660df488527f9c0f0ab980
Austin Chao <austinschao@gmail.com>
2022-08-03 13:59:12 -0700

    sumPossible

--- dynamicprogramming.js
+++ dynamicprogramming.js
@@ -11,11 +11,30 @@ Solve this recursively.
 */
 const tribonacci = (n, memo = {}) => {
   if (n in memo) return memo[n];
-  if (n === 0) return 0;
-  if (n === 1) return 0;
+  if (n === 0 || n === 1) return 0;
   if (n === 2) return 1;
 
   memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
 
   return memo[n];
+};
+
+/*
+Write a function sumPossible that takes in an amount and an array of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array. You may reuse numbers of the array as many times as necessary.
+
+You may assume that the target amount is non-negative.
+*/
+const sumPossible = (amount, numbers, memo = {}) => {
+  if (amount === 0) return true;
+  if (amount < 0) return false;
+  if (amount in memo) return memo[amount];
+
+  for (let num of numbers) {
+    if ((sumPossible(amount - num, numbers, memo))) {
+      memo[amount] = true;
+      return true;
+    }
+    memo[amount] = false;
+  }
+  return false;
 };
