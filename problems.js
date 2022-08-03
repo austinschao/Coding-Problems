467f0be08e06c021bd0aff42663fd40471048127
Austin Chao <austinschao@gmail.com>
2022-08-02 18:13:32 -0700

    141. Linked List Cycle

--- problems.js
+++ problems.js
@@ -1136,3 +1136,41 @@ const isHappy = (n, visited = new Set()) => {
 const findSum = (nums) => {
   return nums.reduce((prev, curr) => prev + curr);
 };
+
+/*
+141. Linked List Cycle
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+const hasCycle = (head) => {
+  const visited = new Set();
+  if (findCycle(head, visited)) {
+    return true;
+  }
+  return false;
+
+  // let fast = head
+  // while (fast && fast.next) {
+  //     head = head.next
+  //     fast = fast.next.next
+  //     if (head === fast) {
+  //         return true;
+  //     }
+  // }
+  // return false;
+};
+
+const findCycle = (node, visited) => {
+  if (!node) return false;
+  if (visited.has(node)) return true;
+
+  visited.add(node);
+  if (findCycle(node.next, visited)) {
+    return true;
+  }
+  return false;
+};
