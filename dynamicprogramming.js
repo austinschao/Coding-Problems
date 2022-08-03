35b137c3aa0f04c74d18a499236c04a79d2184b3
Austin Chao <austinschao@gmail.com>
2022-08-03 13:29:18 -0700

    Tribonnaci

--- dynamicprogramming.js
+++ dynamicprogramming.js
@@ -15,7 +15,7 @@ const tribonacci = (n, memo = {}) => {
   if (n === 1) return 0;
   if (n === 2) return 1;
 
-  memo[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
 
   return memo[n];
 };
