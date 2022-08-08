7f6f419b831649f245b46eac7c348166f4b2a413
Austin Chao <austinschao@gmail.com>
2022-08-08 13:45:16 -0700

    max path sum

--- dynamicprogramming.js
+++ dynamicprogramming.js
@@ -38,3 +38,22 @@ const sumPossible = (amount, numbers, memo = {}) => {
   }
   return false;
 };
+
+/*
+Write a function, maxPathSum, that takes in a grid as an argument. The function should return the maximum sum possible by traveling a path from the top-left corner to the bottom-right corner. You may only travel through the grid by moving down or right.
+
+You can assume that all numbers are non-negative.
+*/
+const maxPathSum = (grid, row = 0, col = 0, memo = {}) => {
+  const pos = row + "," + col;
+  if (pos in memo) return memo[pos];
+
+  if (row === grid.length || col === grid[0].length) return 0;
+  if (row === grid.length - 1 && col === grid[0].length - 1) return grid[row][col];
+
+  let downPath = maxPathSum(grid, row + 1, col, memo);
+  let rightPath = maxPathSum(grid, row, col + 1, memo);
+
+  memo[pos] = grid[row][col] + Math.max(downPath, rightPath);
+  return memo[pos];
+};
