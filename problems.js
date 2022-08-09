bbc39960c20e4b568d263930bbd60627f7457567
Austin Chao <austinschao@gmail.com>
2022-08-09 07:45:24 -0700

    100. Same Tree

--- problems.js
+++ problems.js
@@ -1279,3 +1279,18 @@ const _coinChange = (coins, amount, memo = {}) => {
   memo[amount] = minCoins;
   return minCoins;
 };
+
+/*
+100. Same Tree
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.;
+*/
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
