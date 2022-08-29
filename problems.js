ea057470d34c950b5d50e7512e74b7ec51671cc7
Austin Chao <austinschao@gmail.com>
2022-08-29 11:14:44 -0700

    572. Subtree of Another Tree

--- problems.js
+++ problems.js
@@ -1456,3 +1456,33 @@ const convertToZeroes = (matrix, r, c, visited) => {
     }
   }
 };
+
+/*
+572. Subtree of Another Tree
+
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+*/
+
+const isSubtree = (root, subRoot) => {
+  const stack = [root];
+
+  while (stack.length) {
+    const curr = stack.pop();
+    if (isSame(curr, subRoot)) {
+      return true;
+    }
+    if (curr.left) stack.push(curr.left);
+    if (curr.right) stack.push(curr.right);
+  }
+  return false;
+};
+
+const isSame = (tree1, tree2) => {
+  if (!tree1 && !tree2) return true;
+  if (!tree1 || !tree2) return false;
+  if (tree1.val !== tree2.val) return false;
+
+  return isSame(tree1.left, tree2.left) && isSame(tree1.right, tree2.right);
+};
