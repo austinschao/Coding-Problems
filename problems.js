b79977b32b5cdb3e767573ef2ff24a7bdab5661a
Austin Chao <austinschao@gmail.com>
2022-08-30 08:39:34 -0700

    98. Validate BST

--- problems.js
+++ problems.js
@@ -1599,3 +1599,20 @@ const levelOrderRecursively = (root, distance = 0, result = []) => {
 
   return result;
 };
+
+/*
+98. Validate Binary Search Tree
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+*/
+const isValidBST = (root, left = -Infinity, right = Infinity) => {
+  if (!root) return true;
+  if ((root.val > left && root.val < right) === false) return false;
+  return isValidBST(root.left, left, root.val) && isValidBST(root.right, root.val, right);
+};
